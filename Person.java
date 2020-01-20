25aa27802773028a0862ffcb2cb48e906d3cc57d
Cameron <camotech@vt.edu>
2020-01-20 15:35:57 -0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,26 @@
+public class Person {
+  /**
+  * Three typical examples are Alice, Bob, and Oscar
+  * one will be titled main as a hub
+  */ 
+  
+  private String name;
+  private long;
+  
+  
+  public Person() {
+    name = "Anon";
+  }
+  public Person(String n) {
+    name = n;
+  }
+  
+  public void pubPass(String info) {
+    
+  }
+  
+  public void genKey() {
+    
+  }
+
+}
